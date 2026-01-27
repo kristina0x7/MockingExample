35acb8e308aba4a34cfe2b8bf1ffb2f2fe3180b1
Kristina M <kristina0x7@gmail.com>
2026-01-27 18:08:40 +0100

    add exception for Separation of concern and small improvements of paymentprocessor class

---
+++ src/main/java/com/example/payment/PaymentProcessingException.java
@@ -0,0 +1,10 @@
+package com.example.payment;
+
+public class PaymentProcessingException extends RuntimeException {
+    public PaymentProcessingException(String message) {
+        super(message);
+    }
+
+public PaymentProcessingException(String message, Throwable cause) {
+    super(message, cause);}
+}
